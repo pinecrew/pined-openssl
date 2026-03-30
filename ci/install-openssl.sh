361a7a67143584d939ca1e71083676b7f7dca41e
citrux <369565@gmail.com>
2026-03-30 22:13:04 +0300

    wip: own openssl build [no ci]

---
+++ ci/install-openssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xe
+
+INSTALL_LOCATION=$1
+
+OPENSSL_URL="https://github.com/openssl/openssl/releases/download"
+
+curl -#LO "${OPENSSL_URL}/${OPENSSL_VERSION}/${OPENSSL_VERSION}.tar.gz"
+echo "${OPENSSL_SHA256}  ${OPENSSL_VERSION}.tar.gz" | sha256sum -c -
+tar zxf ${OPENSSL_VERSION}.tar.gz
+pushd ${OPENSSL_VERSION}
+BUILD_FLAGS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit enable-ec_nistp_64_gcc_128"
+./config $BUILD_FLAGS --prefix=${INSTALL_LOCATION} --openssldir=${INSTALL_LOCATION}
+make depend
+make -j4
+# avoid installing the docs
+# https://github.com/openssl/openssl/issues/6685#issuecomment-403838728
+make install_sw install_ssldirs
+popd
