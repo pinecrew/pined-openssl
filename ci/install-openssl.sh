9f03e2495e062bec7fcc68702c269bb98b4f6152
citrux <369565@gmail.com>
2026-04-01 23:14:57 +0300

    ci: still try 2

--- ci/install-openssl.sh
+++ ci/install-openssl.sh
@@ -6,7 +6,7 @@ INSTALL_LOCATION=$1
 OPENSSL_URL="https://github.com/openssl/openssl/releases/download"
 
 curl -#LO "${OPENSSL_URL}/${OPENSSL_VERSION}/${OPENSSL_VERSION}.tar.gz"
-echo "${OPENSSL_SHA256}  ${OPENSSL_VERSION}.tar.gz" | sha256sum -c -
+echo "${OPENSSL_SHA256} ${OPENSSL_VERSION}.tar.gz" | sha256sum -c -
 tar zxf ${OPENSSL_VERSION}.tar.gz
 pushd ${OPENSSL_VERSION}
 ./config ${BUILD_FLAGS} --prefix=${INSTALL_LOCATION} --openssldir=${INSTALL_LOCATION}
